8e3d5eb8a3c91bb66ddb226921583df432e4d6b8
kevin yarasca <kevinyarascaponce@gmail.com>
2017-09-16 19:15:56 -0500

    cambios en el diseño tienda

--- tienda/index.php
+++ tienda/index.php
@@ -27,12 +27,29 @@
 		<div class="col-sm-4 men-wear-left">
 					<img class="img-responsive" src="images/logo.png" alt=" ">
 				</div>
-		<!-- header-bot -->
+
+<!-- header-bot -->
 			<div class="col-md-4 logo_agile">
 				<h1><a href=""> <i class="fa fa-shopping-bag top_logo_agile_bag" aria-hidden="true"></i></a></h1>
 			</div>
         <!-- header-bot -->
 		<div class="col-md-4 agileits-social top_content">
+		<ul class="social-nav model-3d-0 footer-social w3_agile_social">
+		<li class="share">Share On : </li>
+															<li><a href="#" class="facebook">
+																  <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+																  <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
+															<li><a href="#" class="twitter"> 
+																  <div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+																  <div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>
+															<li><a href="#" class="instagram">
+																  <div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>
+																  <div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div></a></li>
+															<li><a href="#" class="pinterest">
+																  <div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
+																  <div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div></a></li>
+														</ul>
+
 						
 						
 		</div>
@@ -369,7 +386,7 @@
 										<img src="images/m1.jpg" alt="" class="pro-image-back">
 											<div class="men-cart-pro">
 												<div class="inner-men-cart-pro">
-													<a href="single.html" class="link-product-add-cart">Vista Rapida</a>
+													
 												</div>
 											</div>
 											<span class="product-new-top">Nuevo</span>
